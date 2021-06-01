6be86c2d199b120af72ae77ae17dd52ef5f5cf56
Yordan Krushkov <yordankrushkov@gmail.com>
2021-06-01 09:38:08 +0100

    website

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   const { isAuthenticated } = context
   const isAuth = isAuthenticated;
   return (
-    <Router>
+    <Router basename="/">
       <div className="App">
         <Header />
         <Background />
